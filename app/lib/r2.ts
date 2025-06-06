756818db2034c7a650007ccd7475bd1b26a94363
ndom91 <yo@ndo.dev>
2025-06-06 23:29:50 +0200

    fix: more cleanup

--- app/lib/r2.ts
+++ app/lib/r2.ts
@@ -11,11 +11,9 @@ export interface FileObject {
 const R2_ACCOUNT_ID = process.env.R2_ACCOUNT_ID!
 const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID!
 const R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY!
-const R2_REGION = process.env.R2_REGION!
-const R2_DEV_URL = process.env.R2_DEV_URL!
 
 export const r2Client = new S3Client({
-  region: R2_REGION ? R2_REGION : 'auto',
+  region: 'auto',
   endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
     accessKeyId: R2_ACCESS_KEY_ID,
