8fd82e3d77cc330b3af5fddba53473220ad2fb65
ndom91 <yo@ndo.dev>
2025-06-06 17:33:42 +0200

    fix: rm unnecessary frame-web files

--- app/lib/auth-client.ts
+++
@@ -1,9 +0,0 @@
-import { createAuthClient } from "better-auth/react"
-import { passkeyClient } from "better-auth/client/plugins"
-
-export const { signIn, signOut, useSession, passkey } = createAuthClient({
-  baseURL: "http://localhost:3000",
-  plugins: [
-    passkeyClient()
-  ]
-})
