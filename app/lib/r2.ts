2b5c0199e0e86757c4a5784dccfe07626d75e4d8
ndom91 <yo@ndo.dev>
2025-07-11 17:45:41 +0200

    Try custom domain

--- app/lib/r2.ts
+++ app/lib/r2.ts
@@ -8,13 +8,14 @@ export interface FileObject {
   StorageClass?: string;
 }
 
-const R2_ACCOUNT_ID = process.env.R2_ACCOUNT_ID!;
+// const R2_ACCOUNT_ID = process.env.R2_ACCOUNT_ID!;
 const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID!;
 const R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY!;
 
 export const r2Client = new S3Client({
   region: "auto",
-  endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  // endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: "https://upload.destinationdomino.wedding",
   credentials: {
     accessKeyId: R2_ACCESS_KEY_ID,
     secretAccessKey: R2_SECRET_ACCESS_KEY,
