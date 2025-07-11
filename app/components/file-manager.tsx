6eea705a68303ffb0dda9da24158e8f74016fc26
ndom91 <yo@ndo.dev>
2025-07-11 17:56:01 +0200

    Update styling

--- app/components/file-manager.tsx
+++ app/components/file-manager.tsx
@@ -201,7 +201,7 @@ export default function FileManager() {
         <div className="flex items-center justify-center w-full">
           <label
             htmlFor="dropzone-file"
-            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-stone-100 dark:bg-gray-700 hover:bg-stone-300 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 transition-all duration-250 ease-in-out"
+            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-stone-100 dark:bg-gray-700 dark:border-gray-600 dark:hover:border-gray-500 hover:bg-stone-200/50 transition-all duration-250 ease-in-out"
           >
             <div className="flex flex-col items-center justify-center pt-5 pb-6">
               <svg
