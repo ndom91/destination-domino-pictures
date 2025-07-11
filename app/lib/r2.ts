27da73be39e0bc8404987284a3e4873430030bd0
ndom91 <yo@ndo.dev>
2025-07-11 18:09:14 +0200

    Revert r2 client endpoint testing

--- app/lib/r2.ts
+++ app/lib/r2.ts
@@ -8,14 +8,13 @@ export interface FileObject {
   StorageClass?: string;
 }
 
-// const R2_ACCOUNT_ID = process.env.R2_ACCOUNT_ID!;
+const R2_ACCOUNT_ID = process.env.R2_ACCOUNT_ID!;
 const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID!;
 const R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY!;
 
 export const r2Client = new S3Client({
   region: "auto",
-  // endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
-  endpoint: "https://upload.destinationdomino.wedding",
+  endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
     accessKeyId: R2_ACCESS_KEY_ID,
     secretAccessKey: R2_SECRET_ACCESS_KEY,
